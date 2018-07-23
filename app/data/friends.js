ba8f6e5876b12903efb7d3df3defd999bfedb0c3
gary h <gary_houck@yahoo.com>
2018-07-23 18:51:26 -0500

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+    {
+        "name": "James Hetfield",
+        "photo": "http://ultimateclassicrock.com/files/2017/11/hetfield.jpg",
+        "scores": [3,2,5,3,1,1,2,4,3,5]
+    },
+    {
+        "name": "Axl Rose",
+        "photo": "https://img.etsystatic.com/il/a0f662/1073527443/il_570xN.1073527443_cjb1.jpg?version=1",
+        "scores": [1,3,2,4,5,3,1,2,3,1]
+    },
+    {
+        "name": "Eddie Van Halen",
+        "photo": "https://oralcancerfoundation.org/wp-content/uploads/2016/09/Eddie-Van-Halen-300x226.jpg",
+        "scores": [5,3,2,4,3,5,1,1,2,3]
+    },
+    {
+        "name": "Ozzy Osbourne",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhydImz5q7Mdk9oV3HbGEd40tTm18UqMOTiiV-d4u4C7B9GJT3",
+        "scores": [1,2,3,4,5,4,3,2,1,3]
+    },
+    {
+        
+        "name": "Scott Ian",
+        "photo": "https://www.tabletmag.com/wp-content/files_mf/storyart_anthrax_82060.png",
+        "scores": [5,5,5,5,5,5,5,5,5,5]
+    }
+];
+
---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// set up html routes
+
+app.get("/", function(req,res) {
+    res.sendFile(path.join(__dirname,"./public/home.html"));
+});
+
+app.get("/survey", function(req,res) {
+    res.sendFile(path.join(__dirname,"./public/survey.html"));
+});
+
+// route for friends JSON
+
+app.get("/api/friends", function(req,res) {
+    res.sendFile(path.join(__dirname, "./app/data/friends.js"));
+});
+
+// post route for friends array
+
+app.post("/api/friends", function(req, res) {
+    var newFriend = req.body.score;  
+    console.log(newFriend);  
+    friends.push(newFriend);
+    res.json(newFriend);
+  });
+
+// set up server listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+
