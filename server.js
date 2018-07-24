658a20242bc0d325e9bf1bcdf994ef84e161f4c1
gary h <gary_houck@yahoo.com>
2018-07-23 19:14:17 -0500

    last update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get("/", function(req,res) {
     res.sendFile(path.join(__dirname,"./public/home.html"));
 });
 
-app.get("/survey", function(req,res) {
+app.get("/public/survey.html", function(req,res) {
     res.sendFile(path.join(__dirname,"./public/survey.html"));
 });
 
